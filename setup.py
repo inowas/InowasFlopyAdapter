b6bbb13650a53d1cfd40809834d6cede4a28b160
Ralf Junghanns <ralf.junghanns@gmail.com>
2020-10-28 15:34:49 +0100

    Upgrade version for testing purposes

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="InowasFlopyAdapter",
-    version="1.1.4",
+    version="1.1.5",
     author="Ralf Junghanns",
     author_email="ralf.junghanns@gmail.com",
     description="A FLOPY wrapper for the INOWAS-platform",
